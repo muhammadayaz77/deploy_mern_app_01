000eded3ab459bcfa7966d037b8c6c9454bef96c
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-25 22:51:03 +0500

    add routes

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,6 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.mjs';
 import dotenv from 'dotenv'
+// router imports
+import ownerRouter from './routes/ownerRouter.mjs'
+import userRouter from './routes/userRouter.mjs'
+import productRouter from './routes/productRouter.mjs'
 dotenv.config();
 let app = express();
 let PORT = process.env.PORT || 3000;
@@ -9,6 +13,9 @@ app.use(express.json());
 app.use(cors());
 connectDB();
 
+app.use('/owners',ownerRouter)
+app.use('/users',userRouter )
+app.use('/products',productRouter)
 
 app.listen(PORT,()=>{
   console.log(`http://localhost:${PORT}`);
