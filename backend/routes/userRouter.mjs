fefaec72d746525a79c5786d4deb1c01f8bb4e8d
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-26 00:59:03 +0500

    add controllers for login , register

--- backend/routes/userRouter.mjs
+++ backend/routes/userRouter.mjs
@@ -1,8 +1,8 @@
 import express from 'express'
+import { login, register } from '../controllers/authControllers.mjs';
 
 let router = express.Router();
 
-router.get("/",(req,res)=>{
-  res.send("working...")
-})
+router.post("/register",register)
+router.post("/login",login)
 export default router
