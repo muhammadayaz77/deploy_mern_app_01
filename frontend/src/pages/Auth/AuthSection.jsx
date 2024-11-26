34a5905f5d054a58e94416f06b2d1af95ff43c02
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-26 22:52:07 +0500

    setup Tailwind+DaisyUI

---
+++ frontend/src/pages/Auth/AuthSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AuthSection() {
+  return (
+    <div className='min-h-full w-full bg-secondary'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default AuthSection
--- frontend/src/pages/Auth/Index.jsx
+++ frontend/src/pages/Auth/Index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import Register from './Register'
-import Login from './Login'
 import { Route,Routes } from 'react-router'
+import AuthSection from './AuthSection'
 
 
 function Index() {
   return (
     <>
+    
     <Routes>
-      <Route path='/register' element={<Register />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<AuthSection />} />
       <Route path='/*' element={<>Page not found</>} />
     </Routes>
     </>
