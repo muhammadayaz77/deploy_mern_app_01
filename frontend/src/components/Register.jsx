819e028334a67cdd6667a89131044cb404fbffc3
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-27 00:56:23 +0500

    add responsiveness , auth ui completed

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Register() {
   return (
-    <div className='bg-[#E0E6EC] h-[100vh] flex items-center'>
-    <div className='mx-12'>
+    <div className='lg:bg-[#E0E6EC] lg:h-[100vh] lg:mt-0 mt-52 flex items-center'>
+    <div className='lg:mx-12'>
       <h1 className='text-2xl font-normal tracking-tight'>Welcome to <span className='text-blue-500 font-bold'>Scatch</span></h1>
       <h3 className='font-semibold my-2'>Create Your Account</h3>
-      <input type="text" className='p-2 border-2 border-transparent w-full outline-none mb-3    ' placeholder='Full Name' />
-      <input type="text" className='p-2 border-2 border-transparent w-full outline-none mb-3' placeholder='Email' />
-      <input type="text" className='p-2 border-2 border-transparent w-full outline-none mb-3' placeholder='Password' />
+      <input type="text" className='p-2 border-2 border-transparent lg:bg-white bg-[#E0E6EC] w-full outline-none mb-3    ' placeholder='Full Name' />
+      <input type="text" className='p-2 border-2 border-transparent lg:bg-white bg-[#E0E6EC] w-full outline-none mb-3' placeholder='Email' />
+      <input type="text" className='p-2 border-2 border-transparent lg:bg-white bg-[#E0E6EC] w-full outline-none mb-3' placeholder='Password' />
       <button className='bg-blue-500 p-3 py-2 text-white font-medium tracking-wider text-sm rounded-3xl'>Create My Account</button>
     </div>
     </div>
--- frontend/src/pages/Auth/AuthSection.jsx
+++ frontend/src/pages/Auth/AuthSection.jsx
@@ -5,15 +5,15 @@ import Or from '../../components/Or'
 
 function AuthSection() {
   return (
-    <div className='grid grid-cols-12 min-h-[100vh] w-[80%] mx-auto'>
-      <div className='px-[50px] relative col-span-5'>
+    <div className='grid grid-cols-12 min-h-[100vh] md:w-[80%] mx-auto'>
+      <div className='sm:px-[50px] px-5 relative col-span-12 lg:col-span-5'>
         <Register />
         <h1 className='text-zinc-600 font-bold text-3xl absolute top-7 left-[10px]'>Scatch</h1>
       </div>
-      <div className='col-span-2 flex justify-center items-center'>
+      <div className='col-span-12 lg:col-span-2 flex justify-center items-center md:my-0 my-10'>
       <Or></Or>
       </div>
-      <div className='px-[50px] col-span-5'>
+      <div className='sm:px-[50px] px-5 col-span-12 lg:col-span-5'>
         <Login />
       </div>
     </div>
