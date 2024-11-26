ac9564070356789f0b4b1e916e4bbaab750cfc16
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-11-26 23:26:18 +0500

    add register ui

--- frontend/src/pages/Auth/AuthSection.jsx
+++ frontend/src/pages/Auth/AuthSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Register from '../../components/Register'
 
 function AuthSection() {
   return (
-    <div className='min-h-full w-full bg-secondary'>
-      <div>
-
+    <div className='grid grid-cols-2 min-h-[100vh] w-full bg-while'>
+      <div className='px-[150px] relative'>
+        <Register />
+        <h1 className='text-zinc-600 font-bold text-3xl absolute top-5 left-[115px]'>Scatch</h1>
       </div>
     </div>
   )
