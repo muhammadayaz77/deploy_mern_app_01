c045248297ba2d4e8dc5fe55978c4e59b7d60381
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-12-03 20:47:37 +0500

    remove cosoles

--- backend/routes/authRouter.mjs
+++ backend/routes/authRouter.mjs
@@ -8,9 +8,7 @@ let router = express.Router();
 router.get('/verify-admin', isLoginUser,verifyAdmin, async(req, res) => {
   try {
     const user = req.user._id; // Decoded from the token
-    console.log(user);
     let existedUser = await userModel.findById({_id : user});
-    console.log(existedUser);
     if (existedUser.isadmin) {
       res.status(200).json({ isadmin : true, message : 'You can Access' });
     } else if(!existedUser.isadmin) {
