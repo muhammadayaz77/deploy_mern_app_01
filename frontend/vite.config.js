4c17742b56ee9f393bf89cddc16856e1bbe8d073
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-12-13 16:35:55 +0500

    changes in vite.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // Ensure your base path is set to root for proper routing
+  build: {
+    outDir: 'build', // Output folder for your production build (default: dist)
+  },
   plugins: [react()],
 })
+
+// export default defineConfig({
+//   base: '/',  // Ensure your base path is set to root for proper routing
+//   build: {
+//     outDir: 'build', // Output folder for your production build (default: dist)
+//   }
+// });
