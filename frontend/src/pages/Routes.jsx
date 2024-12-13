d1d8f4f5e12c413d5173135c32bb9432ea958e67
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-12-13 17:06:15 +0500

    changes in Auth

--- frontend/src/pages/Routes.jsx
+++ frontend/src/pages/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Route,Routes } from 'react-router-dom'
-import Auth from './Auth'
+import Auth from './Auth/Index.jsx'
 import Home from './Home'
 import { useSelector } from 'react-redux'
 import ProtectedRouter from '../protectedRouter/ProtectedRouter'
