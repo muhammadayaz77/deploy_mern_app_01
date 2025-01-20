38ba2017f6d0f4bedd562b2b795e68776762aedc
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2025-01-20 23:36:11 +0500

    refactor: update Stripe import and configuration; add GSAP animations to Or component

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,15 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db.mjs';
 import dotenv from 'dotenv'
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 // router imports
 import userRouter from './routes/userRouter.mjs'
 import productRouter from './routes/productRouter.mjs'
 import authRouter from './routes/authRouter.mjs'
 import cartRouter from './routes/cartRouter.mjs'
 dotenv.config();
-const Stripe = require('stripe');
-const stripe = Stripe('sk_test_51QVmyJGO9zJL4clfK58gcpU9wmCgbtWI2Po8Z0rYLP8N2Cu8l3VV9ozJMBkNevYfljvf8YOqGJBOvpgPuq3f4RGi00IFeOgWia');
 
 let app = express();
 let PORT = process.env.PORT || 3000;
--- frontend/src/components/Or.jsx
+++ frontend/src/components/Or.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useRef } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
 
 function Or() {
+  useGSAP(() => {
+    gsap.to('#or',{
+      rotate : 360,
+      repeat : -1,
+      duration : 2,
+      ease : 'linear'
+    })
+  })
   return (
-    <div className='bg-blue-500 text-white font-bold w-10 h-10 flex justify-center items-center rounded-full'>or</div>
+    <div id='or' className='bg-blue-500 text-white font-bold w-10 h-10 flex justify-center items-center rounded-full'>
+      <div>
+      or
+      </div>
+    </div>
   )
 }
 
